fbfc34ddb4a3bf9b2d4e9186107cd36aeee01920
matt-yst <matthew.ys.tan@gmail.com>
2026-08-11 17:52:31 +0000

    Rewriting structural test

--- tests/test_structural_validation.py
+++ tests/test_structural_validation.py
@@ -5,22 +5,16 @@ import pydantic
 import json
 
 
-output_dir = Path("shelly_test_outputs").resolve()
+def _load_all_cases():
+    output_dir = Path("shelly_test_outputs").resolve()
+    all_cases = []
+    for child in output_dir.iterdir():
+        with child.open("r") as f:
+            cases = json.load(f)
+        all_cases.extend(cases)
+    return all_cases
 
 
-# def _load_cases():
-#     path = Path(__file__).resolve().parents[1] / "shelly_test_outputs" / "event_output.json"
-#     with path.open() as f:
-#         return json.load(f)
-
-# @pytest.mark.parametrize("test_case", _load_cases(), ids=lambda x: x["name"])
-# def test_schema_validity(test_case):
-#     TestScenario.model_validate(test_case)
-
-for child in output_dir.iterdir():
-    with open(child, 'r') as f:
-        test_cases = json.load(f)
-
-    @pytest.mark.parametrize("test_case", test_cases, ids=lambda x: x["name"])
-    def test_schema_validity(test_case):
-        TestScenario.model_validate(test_case)
+@pytest.mark.parametrize("test_case", _load_all_cases(), ids=lambda x: x["name"])
+def test_schema_validity(test_case):
+    TestScenario.model_validate(test_case)
