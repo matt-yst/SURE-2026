ca2c5689387f3212e4b7aa7f0cc1490ac872be7c
matt-yst <matthew.ys.tan@gmail.com>
2026-08-11 17:20:24 +0000

    2nd output shelly

--- tests/test_structural_validation.py
+++ tests/test_structural_validation.py
@@ -1,29 +1,27 @@
 import pytest
 from pathlib import Path
-# from docs_parser.repo_parser import TestScenario
+from docs_parser.repo_parser import TestScenario
 import pydantic 
 import json
 
 
-def test_try():
-    assert True
+output_dir = Path("shelly_test_outputs").resolve()
 
 
-# def _load_cases():
-#     path = Path(__file__).resolve().parents[1] / "shelly_test_outputs" / "event_output.json"
-#     with path.open() as f:
-#         return json.load(f)
+def _load_cases():
+    path = Path(__file__).resolve().parents[1] / "shelly_test_outputs" / "event_output.json"
+    with path.open() as f:
+        return json.load(f)
 
-# @pytest.mark.parametrize("test_case", _load_cases(), ids=lambda x: x["name"])
-# def test_schema_validity(test_case):
-#     TestScenario.model_validate(test_case)
+@pytest.mark.parametrize("test_case", _load_cases(), ids=lambda x: x["name"])
+def test_schema_validity(test_case):
+    TestScenario.model_validate(test_case)
 
 # for child in output_dir.iterdir():
-#     print(child.name)
-#     print("\n")
-#     with open(path, 'r') as f:
+#     with open(child, 'r') as f:
 #         test_cases = json.load(f)
+#     print(test_cases)
 
-#     @pytest.mark.parametrize("test_case", test_cases, ids=lambda x: x["name"])
-#     def test_schema_validity(test_case):
-#         TestScenario.model_validate(test_case)
+    # @pytest.mark.parametrize("test_case", test_cases, ids=lambda x: x["name"])
+    # def test_schema_validity(test_case):
+    #     TestScenario.model_validate(test_case)
